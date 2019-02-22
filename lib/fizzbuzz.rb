1ec8dafd6f4088faa6d869b13ce354316cd12b51
Samantha Ixer <samantha.ixer@johnlewis.co.uk>
2019-02-22 10:35:58 +0000

    added tests for fizz and moving on to buzzzzzzzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
-def fizzbuzz(number)
-  "fizz"
+class Integer
+  def fizzbuzz
+    return "fizz" if self % 3 == 0
+    return "buzz" if self == 5
+    return self
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,19 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "4" when passed 4' do
+    expect(4.fizzbuzz).to eq 4
+  end
+
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(6.fizzbuzz).to eq 'fizz'
+    expect(12.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
   end
 end
